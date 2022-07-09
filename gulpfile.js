85813ef443c93e2e78bfa42658927f7afd63e1d5
kempo <kemal.aslan@hackuva.de>
2022-07-09 21:21:08 +0200

    npm run deploy

--- gulpfile.js
+++ gulpfile.js
@@ -11,65 +11,79 @@ var purgecss = require('gulp-purgecss');
 
 
 // Clean output directory
-gulp.task('clean', function () {
-  return del(['docs']);
+gulp.task('clean', function() {
+    return del(['docs']);
 });
 
 // Gulp task to minify CSS files
-gulp.task('styles', function () {
+gulp.task('styles', function() {
     return gulp.src('./css/style.css')
-      // Auto-prefix css styles for cross browser compatibility
-      .pipe(autoprefixer({}))
-      // Minify the file
-      .pipe(csso({
-        restructure: false
-      }))
-      // Output
-      .pipe(gulp.dest('./docs/css'))
-  });
+        // Auto-prefix css styles for cross browser compatibility
+        .pipe(autoprefixer({}))
+        // Minify the file
+        .pipe(csso({
+            restructure: false
+        }))
+        // Output
+        .pipe(gulp.dest('./docs/css'))
+});
 
 // Gulp task to minify JavaScript files
 gulp.task('scripts', function() {
     return gulp.src('./js/**/*.js')
-      // Minify the file
-      .pipe(uglify())
-      // Output
-      .pipe(gulp.dest('./docs/js'))
-  });
+        // Minify the file
+        .pipe(uglify())
+        // Output
+        .pipe(gulp.dest('./docs/js'))
+});
+
+// minify the html files in the ./gartentoilette folder and copy them to the docs folder
+gulp.task('subpages', function() {
+    return gulp.src('./gartentoilette/*.html', { base: './' })
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true
+        }))
+        .pipe(gulp.dest('./docs'));
+})
 
 // Gulp task to minify HTML files
 gulp.task('pages', function() {
-    return gulp.src(['*.html'])
-      .pipe(htmlmin({
-        collapseWhitespace: true,
-        removeComments: true
-      }))
-      .pipe(gulp.dest('./docs'));
-  });
+    return gulp.src(['*.html'], { base: '.' })
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true
+        }))
+        .pipe(gulp.dest('./docs'));
+});
 
 // Gulp task to minify images
-gulp.task('images', function () {
+gulp.task('images', function() {
     return gulp.src('images/**/*')
-     .pipe(imagemin())
-     .pipe(gulp.dest('docs/images'));
+        .pipe(imagemin())
+        .pipe(gulp.dest('docs/images'));
 });
 
 gulp.task('copy-plugins', function() {
-  return gulp.src('plugins/**/*')
-   .pipe(gulp.dest('docs/plugins/'));
+    return gulp.src('plugins/**/*')
+        .pipe(gulp.dest('docs/plugins/'));
 });
 
 gulp.task('copySEOfiles', function() {
-  return gulp.src(['robots.txt', 'sitemap.xml', 'CNAME'])
-   .pipe(gulp.dest('docs/'));
+    return gulp.src(['robots.txt', 'sitemap.xml', 'CNAME'])
+        .pipe(gulp.dest('docs/'));
 });
 
 gulp.task('purgecss', () => {
-  return gulp.src('css/*.css')
-      .pipe(purgecss({
-          content: ['*.html']
-      }))
-      .pipe(gulp.dest('docs/css'))
+    return gulp.src('css/*.css')
+        .pipe(purgecss({
+            content: ['*.html']
+        }))
+        .pipe(gulp.dest('docs/css'))
 })
 
-gulp.task('default', gulp.series('clean', 'purgecss', 'copySEOfiles', 'copy-plugins', 'images', 'pages', 'scripts', 'styles'))
+gulp.task('default', gulp.series('clean', 'purgecss', 'copySEOfiles', 'copy-plugins', 'images', 'pages', 'subpages', 'scripts', 'styles'))
