20896279fbbd279e2d050c213d8a7301e39ed2a2
outdoorhosen24-de <outdoorhosen24.de@proton.me>
2022-10-23 02:05:59 +0200

    deploying

---
+++ pages/trelino-evo-s.vue
@@ -0,0 +1,256 @@
+<template>
+    <div>
+        <section class="page-title bg-1">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="block text-center">
+                            <span class="text-white font-weight-bold text-lg"> Ultrakompakte und leichte
+                                Trenntoilette</span>
+                            <h1 class="text-capitalize mb-5 text-lg">Trelino Evo S</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section department-single">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="department-img">
+                            <img src="/images/trenntoiletten/trelino-evo-s.webp" alt="Trelino Evo S"
+                                style="height: 50%; width: 50%" class="img-fluid" />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="department-content mt-5">
+                            <h2 class="text-md">Trelino Evo S</h2>
+                            <div class="divider my-4"></div>
+                            <p class="lead">
+                                Die sehr leichte und kompakte Trenntoilette von Trelino mit dem Namen Evo S ist eine
+                                mobile Trenntoilette, die für den Einsatz unterwegs kreiert worden ist. Sie zeichnet
+                                sich durch ihr geringes Gewicht und kleine Größe aus. Insgesamt weist diese kleine
+                                Trenntoilette lediglich ein Gewicht von 4,3 KG auf.
+                            </p>
+                            <p>
+                                Vorgestellt wurde die Trelino Evo S das erste mal auf der Caravan Salon 2022 Messe in
+                                Düsseldorf. Hervorheben tut sich die Trenntoilette vor allem durch ihre Kompaktheit. Die
+                                Trelino Evo S ist also für überall dort gedacht, wo wenig Platz verfügbar ist, zum
+                                Beispiel beim Camping, im Boot oder im Van.
+                            </p>
+
+                            <p>Das Material der mobilen Trenntoilette ist Kunststoff. Funktionieren tut die Trelino Evo
+                                S über einen Trenneinsatz, welcher ebenfalls aus Kunststoff ist. Der Deckel der
+                                Trenntoilette hat eine Softclose-Automatik, welches wir als ein nettes komfortables
+                                Feature betrachten.</p>
+
+                            <p>Die Sitzhöhe der Trelino Evo S beträgt lediglich 29,00 cm, was für den Transport
+                                natürlich optimal ist, für den Stuhlgang jedoch suboptimal und sicherlich
+                                gewöhnungsbedürftig.</p>
+
+                            <p>Testen konnten wir die Trenntoilette bisher leider nicht, da sie derzeit nur
+                                vorbestellbar ist. Die Maße beträgt: Breite 33,00 cm, Tiefe 39,00 cm, Höhe 29,00 cm. Das
+                                Gewicht beträgt lediglich 4,3 KG. Somit ist die kompakte Trenntoilette eine der
+                                leichtesten mobilen Toiletten die verfügbar sind.</p>
+
+                            <p>Das Volumen des Urinkanisters beträgt 5 Liter, die des Feststoffbehälters beträgt 6
+                                Liter. Eine gesonderte Belüftung hat die kleine Evo S von Trelino nicht, somit ist eine
+                                Verwendung von Einstreu definitiv zu empfehlen.</p>
+
+                            <p>Die Evo S benötigt weder Wasser, Strom, noch Chemie und bietet damit eine Unabhängigkeit,
+                                was sie ideal macht für Unternehmungen wie Camping, Wochenendtrips oder den Transport im
+                                PKW.</p>
+
+                            <p>Trelino selbst schreibt über sein Produkt, das es solide gebaut ist und somit unterwegs
+                                eine Auslaufsicherheit garantiert ist. Ob Trelino sein versprechen hält, ist abzuwarten.
+                            </p>
+
+                            <p>Der Korpus der Mini-Trenntoilette besteht aus Kunststoff, Acrylnitril-Butadien-Styrol
+                                (ABS) um genau zu sein. Die Behälter bestehen aus Polypropylen und Polyethylen, welches
+                                ebenfalls Kunststoff ist.</p>
+
+                            <p>Wir finden das Design der Trelino Evo S gut, sie hat einen cleanes, minimalistisches
+                                Design. </p>
+
+                            <p>Vorhanden ist die kleine Trenntoilette in den Farben weiß, Anthrazit und Grau. Der Preis
+                                liegt bei 349,00 EUR, was wir als hoch angesetzt empfinden. Für alle Vorbesteller bietet
+                                Trelino jedoch einen Rabatt von 50,00 EUR an und ist aktuell somit für 299,00 EUR
+                                vorbestellbar.</p>
+
+                            <p>Der Lieferumfang enthalten sind:</p>
+                            <ul>
+                                <li>Korpus</li>
+                                <li>befestigter Deckel</li>
+                                <li>Toilettensitz</li>
+                                <li>Trenneinsatz</li>
+                                <li>Urinkanister mit Deckel</li>
+                                <li>Deckel mit Membranverschluss</li>
+                                <li>Feststoffbehälter</li>
+                            </ul>
+
+                            <p>
+                                Als Zubehör sind Membrane für den Toilettensitz erhältlich, sowie ein Premium-Zubehör
+                                Set
+                                mit 10 oder 30 Liter Beuteln.
+                            </p>
+
+                            <p>
+                                Wir sind gespannt auf die ultrakompakte Trenntoilette Evo S aus dem Hause Trelino,
+                                welches
+                                ab Februar 2023 ausgeliefert werden soll und machen ein informelles Update hier auf
+                                Trenntoilette.net verfügbar, sobald wir das Produkt erhalten haben.
+                            </p>
+
+                            <h3 class="mt-5 mb-4">Test Kriterien</h3>
+                            <div class="divider my-4"></div>
+                            <ul class="list-unstyled department-service lead">
+                                <li>
+                                    <img src="/images/icons/check.webp" /> Qualität des Materials
+                                </li>
+                                <li><img src="/images/icons/check.webp" /> Sitzkomfort</li>
+                                <li>
+                                    <img src="/images/icons/check.webp" /> Möglichkeiten für
+                                    Ersatzteile
+                                </li>
+                                <li>
+                                    <img src="/images/icons/check.webp" /> Geruchsentwicklung
+                                </li>
+                                <li><img src="/images/icons/check.webp" /> Entleerung</li>
+                                <li><img src="/images/icons/check.webp" /> Preis</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="sidebar-widget schedule-widget mt-5">
+                            <h5 class="mb-4">Evo S</h5>
+
+                            <ul class="list-unstyled">
+                                <li class="d-flex justify-content-between align-items-center">
+                                    <a href="#">Marke</a>
+                                    <span>Trelino</span>
+                                </li>
+                                <li class="d-flex justify-content-between align-items-center">
+                                    <a href="#">Material</a>
+                                    <span>Kunststoff</span>
+                                </li>
+                                <li class="d-flex justify-content-between align-items-center">
+                                    <a href="#">Maße</a>
+                                    <span>Höhe 290 - Tiefe 390 - Breite 330 mm </span>
+                                </li>
+                                <li class="d-flex justify-content-between align-items-center">
+                                    <a href="#">Gewicht</a>
+                                    <span>4,3 KG</span>
+                                </li>
+                            </ul>
+
+                            <div class="sidebar-contatct-info mt-4">
+                                <p class="mb-0">Preis</p>
+                                <h3>299,00 EUR</h3>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <a href="https://www.trelino.com/de/products/trelino%C2%AE-evo-s?variant=43660998377736"
+                                target="_blank" rel="nofollow noopener" title="Trelino Evo S online bestellen"
+                                class="btn btn-main-2 btn-round-full">Online
+                                Bestellen ></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "TrelinoEvoS",
+    head: {
+        title: "Trelin Evo S - Testbericht",
+        meta: [
+            {
+                hid: "author",
+                name: "author",
+                content: "trenntoilette.net",
+            },
+            {
+                hid: "description",
+                name: "description",
+                content:
+                    "Ultraleichte und ultrakompakte Trenntoilette Evo S von Trelino im Test. Erfahre alles über die Trenntoilette Evo S von Trelino.",
+            },
+        ],
+    },
+    jsonld() {
+        return {
+            "@context": "https://schema.org",
+            "@graph": [
+                {
+                    "@context": "https://schema.org/",
+                    "@type": "Product",
+                    name: "Trelino Evo S",
+                    image: [
+                        "https://www.trenntoilette.net/images/trenntoiletten/trelino-evo-s.webp",
+                    ],
+                    description:
+                        "Ultraleichte und ultrakompakte Trenntoilette Evo S von Trelino im Test. Erfahre alles über die Trenntoilette Evo S von Trelino.",
+                    sku: "unbekannt",
+                    // mpn: "24542",
+                    brand: {
+                        "@type": "Brand",
+                        name: "Trelino",
+                    },
+                    review: {
+                        "@type": "Review",
+                        reviewRating: {
+                            "@type": "Rating",
+                            ratingValue: "4",
+                            bestRating: "4",
+                        },
+                        author: {
+                            "@type": "Organization",
+                            name: "Trenntoilette.net",
+                        },
+                    },
+                    aggregateRating: {
+                        "@type": "AggregateRating",
+                        ratingValue: "4.4",
+                        reviewCount: "1",
+                    },
+                    offers: {
+                        "@type": "Offer",
+                        url: "https://www.trenntoilette.net/trelino-evo-s/",
+                        priceCurrency: "EUR",
+                        price: "299.00",
+                        priceValidUntil: "2023-11-20",
+                        itemCondition: "https://schema.org/NewCondition",
+                        availability: "https://schema.org/InStock",
+                    },
+                },
+                {
+                    "@context": "https://schema.org",
+                    "@type": "BreadcrumbList",
+                    itemListElement: [
+                        {
+                            "@type": "ListItem",
+                            position: 1,
+                            name: "Testsieger",
+                            item: "https://www.trenntoilette.net/",
+                        },
+                        {
+                            "@type": "ListItem",
+                            position: 2,
+                            name: "Trelino Evo S",
+                        },
+                    ],
+                },
+            ],
+        };
+    },
+};
+</script>
