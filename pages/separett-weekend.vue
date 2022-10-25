ffb206a291a3a77a4d475745c7b29b417e52d6a3
trenntoilette <trenntoilette@protonmail.com>
2022-10-25 18:00:34 +0200

    deploying

---
+++ pages/separett-weekend.vue
@@ -0,0 +1,281 @@
+<template>
+  <div>
+    <section class="page-title bg-1">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="block text-center">
+              <span class="text-white font-weight-bold text-lg"
+                >Trenntoilette mit integriertem 12 Volt Ventilator</span
+              >
+              <h1 class="text-capitalize mb-5 text-lg">Separett Weekend</h1>
+
+              <!-- <ul class="list-inline breadcumb-nav">
+            <li class="list-inline-item"><a href="index.html" class="text-white">Home</a></li>
+            <li class="list-inline-item"><span class="text-white">/</span></li>
+            <li class="list-inline-item"><a href="#" class="text-white-50">Department Details</a></li>
+          </ul> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section department-single">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="department-img">
+              <img
+                src="/images/trenntoiletten/trenntoilette-separett-weekend.webp"
+                alt="Separett Weekend"
+                class="img-fluid"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="department-content mt-5">
+              <h2 class="text-md">Separett Weekend</h2>
+              <div class="divider my-4"></div>
+              <p class="lead">
+                Die Separett Weekend ist eine Trenntoilette der Marke Separett.
+                Sie ist ausgestattet mit einem 12 Volt Ventilator, welcher über
+                einen Stromanschluss, einem Solarpanel, einer Autobatterie oder
+                einem 12 Volt Akku betrieben werden kann.
+              </p>
+              <p>
+                Die Weekend Trenntoilette von Separett besitzt wie die meisten
+                Trenntoiletten, einen Trenneinsatz, welcher Urin von den
+                Feststoffen trennt. Die Separett besitzt einen 12V Lüfter,
+                welcher aber nicht zwingend benutzt werden muss, somit ist eine
+                Stromquelle nicht zwingend notwendig. Im Betrieb konnten wir
+                eine Lautstärke des Lüfters von 31 Dezibel messen, somit ist der
+                Lüfter Lautstärke -technisch nicht negativ aufgefallen.
+              </p>
+
+              <p>
+                Wenn der integrierte Ventilator nicht genutzt wird, ist die
+                Geruchsentwicklung dennoch gering, da zum einen, wie bereits
+                erwähnt, der Urin und die Feststoffe voneinander getrennt
+                werden, zum anderen, weil die Separett Weekend einen
+                Luftdurchzug besitzt, welcher ähnlich funktioniert wie der
+                Luftdurchzug bei einem Kamin.
+              </p>
+
+              <p>
+                Vom Design her ähnelt die Weekend sehr einer herkömmlichen
+                Trenntoilette, sie fällt somit optisch nicht auf. In der
+                Trenntoilette befindet sich der Trenneinsatz, darunter ein
+                Behälter für die Feststoffe. Der Urin wird über einen Schlauch
+                in einen externen Behälter geleitet, welches bei uns zu einem
+                Punktabzug geführt hat, da dies von anderen Produkten besser
+                gelöst ist und diese schlichtweg zwei Behälter im Inneren der
+                Trenntoilette tragen. Ebenfalls führt dieses Konzept natürlich
+                dazu, dass die Trenntoilette nicht für den häufigen Wechsel
+                seines Ortes gemacht ist, sondern eher als feststehende
+                Trenntoilette zu betrachten ist.
+              </p>
+
+              <p>
+                Das Material der Weekend von Separett ist recyclebares
+                Polystyren, sprich Kunststoff und leider kein Holz wie z.B. die
+                TeraBloem von
+                <nuxt-link to="/hersteller/trobolo/" title="TROBOLO"
+                  >TROBOLO</nuxt-link
+                >. Auch dies führt bei uns zu einem Punktabzug, da Holz
+                angenehmer, nachhaltiger und besser für die Umwelt ist, was
+                besser zu der Philosophie einer nachhaltigen, umweltbewussten
+                Trenntoilette passt.
+              </p>
+
+              <p>
+                Wasser oder Chemie sind aber auch für die Separett Weekend nicht
+                notwendig.
+              </p>
+
+              <p>
+                Als Zubehör liefert Separett zwei Sammelbehälter mit Deckel,
+                einen 2 m Urinschlauch zum Abführen in den externen
+                Urinbehälter, ein Abluftrohr, Übergangsstück, eine Rolle
+                kompostierbarer Hygienebeutel für den Einsatz im Inneren, sowie
+                einen Netzadapter welcher für den Anschluss an ein 230 Volt
+                Stromnetz notwendig ist, um den integrierten 12 Volt Ventilator
+                zu verwenden.
+              </p>
+
+              <p>
+                Die Maße der Weekend beträgt: 45 cm in der Breite, 54 cm in der
+                Tiefe, und 52 cm in der Höhe, welches somit auch die Sitzhöhe
+                ist.
+              </p>
+
+              <p>
+                Insgesamt hat uns die Separett Weekend nur mittelmäßig gefallen
+                und ist daher von uns nicht zu empfehlen, zumal der Preis von
+                640 CHF, was umgerechnet ca. 647,00 EUR sind, sehr hoch
+                angesetzt ist. Für diesen Preis finden sich definitiv bessere
+                Produkte, wie die
+                <nuxt-link
+                  to="/trobolo-terabloem/"
+                  title="TROBOLO TeraBloem Testbericht"
+                  >TROBOLO TeraBloem</nuxt-link
+                >
+                aus Holz, für die wir ebenfalls einen Testbericht online haben.
+              </p>
+
+              <h3 class="mt-5 mb-4">Test Kriterien</h3>
+              <div class="divider my-4"></div>
+              <ul class="list-unstyled department-service lead">
+                <li>
+                  <img src="/images/icons/check.webp" /> Qualität des Materials
+                </li>
+                <li><img src="/images/icons/check.webp" /> Sitzkomfort</li>
+                <li>
+                  <img src="/images/icons/check.webp" /> Möglichkeiten für
+                  Ersatzteile
+                </li>
+                <li>
+                  <img src="/images/icons/check.webp" /> Geruchsentwicklung
+                </li>
+                <li><img src="/images/icons/check.webp" /> Entleerung</li>
+                <li><img src="/images/icons/check.webp" /> Preis</li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-lg-4">
+            <div class="sidebar-widget schedule-widget mt-5">
+              <h5 class="mb-4">Separett Weekend</h5>
+
+              <ul class="list-unstyled">
+                <li class="d-flex justify-content-between align-items-center">
+                  <a href="#">Marke</a>
+                  <span>Separett</span>
+                </li>
+                <li class="d-flex justify-content-between align-items-center">
+                  <a href="#">Material</a>
+                  <span>Polystyren (Kunststoff)</span>
+                </li>
+                <li class="d-flex justify-content-between align-items-center">
+                  <a href="#">Maße</a>
+                  <span>Höhe 520 - Tiefe 540 - Breite 450</span>
+                </li>
+                <li class="d-flex justify-content-between align-items-center">
+                  <a href="#">Gewicht</a>
+                  <span>9 KG</span>
+                </li>
+              </ul>
+
+              <div class="sidebar-contatct-info mt-4">
+                <p class="mb-0">Preis</p>
+                <h3>647,62 EUR</h3>
+              </div>
+            </div>
+            <div class="text-center">
+              <a
+                href="https://www.kompotoi.ch/produkt/separett-weekend/"
+                target="_blank"
+                rel="nofollow noopener"
+                title="TeraBloem online bestellen"
+                class="btn btn-main-2 btn-round-full"
+                >Online Bestellen ></a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SeparettWeekend",
+  head: {
+    title: "Separett Weekend Testbericht",
+    meta: [
+      {
+        hid: "author",
+        name: "author",
+        content: "trenntoilette.net",
+      },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Separett Weekend ist eine Trenntoilette mit elektrischem 12V Ventilator. Wir haben das Produkt getestet und veraten dir, worauf geachtet werden muss.",
+      },
+    ],
+  },
+  jsonld() {
+    return {
+      "@context": "https://schema.org",
+      "@graph": [
+        {
+          "@context": "https://schema.org/",
+          "@type": "Product",
+          name: "Separett Weekend",
+          image: [
+            "https://www.trenntoilette.net/images/trenntoiletten/trenntoilette-separett-weekend.webp",
+          ],
+          description:
+            "Separett Weekend, die Trenntoilette mit 12V integriertem, elektrischem Ventilator für den Innenbereich.",
+          sku: "SEP-TOI-WEW",
+          //   mpn: "24542",
+          brand: {
+            "@type": "Brand",
+            name: "Separett",
+          },
+          review: {
+            "@type": "Review",
+            reviewRating: {
+              "@type": "Rating",
+              ratingValue: "3",
+              bestRating: "3",
+            },
+            author: {
+              "@type": "Organization",
+              name: "Trenntoilette.net",
+            },
+          },
+          aggregateRating: {
+            "@type": "AggregateRating",
+            ratingValue: "3.2",
+            reviewCount: "184",
+          },
+          offers: {
+            "@type": "Offer",
+            url: "https://www.trenntoilette.net/separett-weekend/",
+            priceCurrency: "EUR",
+            price: "647.62",
+            priceValidUntil: "2023-11-20",
+            itemCondition: "https://schema.org/NewCondition",
+            availability: "https://schema.org/InStock",
+          },
+        },
+        {
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          itemListElement: [
+            {
+              "@type": "ListItem",
+              position: 1,
+              name: "Testsieger",
+              item: "https://www.trenntoilette.net/",
+            },
+            {
+              "@type": "ListItem",
+              position: 2,
+              name: "Separett Weekend",
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
