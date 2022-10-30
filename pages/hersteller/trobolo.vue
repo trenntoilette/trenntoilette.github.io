4b947592a9c6dfba81c0ee2488b921eb1777c773
kempo <kemal.aslan@hackuva.de>
2022-10-31 00:51:54 +0100

    deploying

--- pages/hersteller/trobolo.vue
+++ pages/hersteller/trobolo.vue
@@ -6,9 +6,7 @@
           <div class="col-lg-6 col-md-12 col-xl-7">
             <div class="block">
               <div class="divider mb-3"></div>
-              <span class="text-uppercase lead letter-spacing text-light"
-                >Trenntoiletten von</span
-              >
+              <span class="text-uppercase lead letter-spacing text-light">Trenntoiletten von</span>
               <h1 class="mb-3 mt-3 text-light">TROBOLO</h1>
 
               <p class="lead mb-4 pr-5 text-light">
@@ -16,11 +14,7 @@
                 herstellt. Die Verarbeitung ist exzellent und das Design ist toll und zeitlos.
               </p>
               <div class="btn-container">
-                <a
-                  href="#article"
-                  class="btn btn-main-2 btn-icon btn-round-full"
-                  >Mehr über TROBOLO erfahren ></a
-                >
+                <a href="#article" class="btn btn-main-2 btn-icon btn-round-full">Mehr über TROBOLO erfahren ></a>
               </div>
             </div>
           </div>
@@ -77,34 +71,44 @@
               <div class="text-md h3 pt-5">Die Trenntoiletten von TROBOLO</div>
 
               <p class="pt-5">
-                <ul>
-                    <li>WandoGo</li>
-                    <li>TeraBloem</li>
-                    <li>KersaBoem</li>
-                    <li>LuweBloem</li>
-                    <li>IndiBloem</li>
-                    <li>KersaBoem & Komoster</li>
-                    <li>LunaBloem</li>
-                    <li>TinyBloem</li>
-                    <li>KitaBoem</li>
-                    <li>NinoBloem</li>
-                    <li>BilaBloem</li>
-                    <li>KitaBoem & Komposter</li>
-                </ul>
+              <ul>
+                <li>WandoGo</li>
+                <li>TeraBloem</li>
+                <li>KersaBoem</li>
+                <li>LuweBloem</li>
+                <li>IndiBloem</li>
+                <li>KersaBoem & Komoster</li>
+                <li>LunaBloem</li>
+                <li>TinyBloem</li>
+                <li>KitaBoem</li>
+                <li>NinoBloem</li>
+                <li>BilaBloem</li>
+                <li>KitaBoem & Komposter</li>
+              </ul>
               </p>
 
               <div class="h4 pt-5">WandaGO</div>
-              <p>Eine extrem leichte und kompakte Campingtoilette. Die Einzige im Programm, bei der Körper aus Kunststoff gefertigt wird.</p>
-              <p><nuxt-link to="/trobolo-wandago/" title="TROBOLO TeraBloem Testbericht">Zum Testbericht der TROBOLO WandaGO</nuxt-link></p>
+              <p>Eine extrem leichte und kompakte Campingtoilette. Die Einzige im Programm, bei der Körper aus
+                Kunststoff gefertigt wird.</p>
+              <p>
+                <nuxt-link to="/trobolo-wandago/" title="TROBOLO TeraBloem Testbericht">Zum Testbericht der TROBOLO
+                  WandaGO</nuxt-link>
+              </p>
 
 
               <div class="h4 pt-5">TeraBloem</div>
               <p>Bausatz für den Innenbereich.</p>
-              <p><nuxt-link to="/trobolo-terabloem/" title="TROBOLO TeraBloem Testbericht">Zum Testbericht der TROBOLO TeraBloem</nuxt-link></p>
+              <p>
+                <nuxt-link to="/trobolo-terabloem/" title="TROBOLO TeraBloem Testbericht">Zum Testbericht der TROBOLO
+                  TeraBloem</nuxt-link>
+              </p>
 
               <div class="h4 pt-5">KersaBoem</div>
               <p>Bausatz für den Außenbereich.</p>
-              <p><nuxt-link to="/trobolo-kersaboem/" title="TROBOLO KersaBoem Testbericht">Zum Testbericht der TROBOLO KersaBoem</nuxt-link></p>
+              <p>
+                <nuxt-link to="/trobolo-kersaboem/" title="TROBOLO KersaBoem Testbericht">Zum Testbericht der TROBOLO
+                  KersaBoem</nuxt-link>
+              </p>
 
 
               <div class="h4 pt-5">LuweBloem</div>
@@ -124,7 +128,10 @@
 
               <div class="h4 pt-5">KitaBoem</div>
               <p>Abgerundet, optional mit Abluftsystem.</p>
-              <p><nuxt-link to="/trobolo-kitaboem/" title="TROBOLO KitaBoem Testbericht">Zum Testbericht der TROBOLO KitaBoem</nuxt-link></p>
+              <p>
+                <nuxt-link to="/trobolo-kitaboem/" title="TROBOLO KitaBoem Testbericht">Zum Testbericht der TROBOLO
+                  KitaBoem</nuxt-link>
+              </p>
 
 
               <div class="h4 pt-5">LunaBloem</div>
@@ -145,12 +152,19 @@
 
 
               <p class="lead">
-                TROBLO ist eine tolle Alternative zu herkömmlichen Toiletten. Das Sortiment ist vielfältig und bietet Trenntoiletten für den Innenbereich, den Außenbereich wie dem <nuxt-link to="/gartentoilette/" title="Gartentoiletten">Garten</nuxt-link>, sowie <nuxt-link to="/campingtoilette/" title="Campingtoiletten">Campingtoiletten</nuxt-link>. Dabei hat jedes Model hat ein Alleinstellungsmerkmal.</p>
-              <p class="lead">Die Verarbeitung der Produkte ist exzellent. Das Holz ist FSC-zertifiziert und stammt aus nachhaltiger Forstwirtschaft.</p> 
+                TROBLO ist eine tolle Alternative zu herkömmlichen Toiletten. Das Sortiment ist vielfältig und bietet
+                Trenntoiletten für den Innenbereich, den Außenbereich wie dem <nuxt-link to="/gartentoilette/"
+                  title="Gartentoiletten">Garten</nuxt-link>, sowie <nuxt-link to="/campingtoilette/"
+                  title="Campingtoiletten">Campingtoiletten</nuxt-link>. Dabei hat jedes Model hat ein
+                Alleinstellungsmerkmal.</p>
+              <p class="lead">Die Verarbeitung der Produkte ist exzellent. Das Holz ist FSC-zertifiziert und stammt aus
+                nachhaltiger Forstwirtschaft.</p>
 
-            <p class="lead">Nicht vernachlässigt hat TROBOLO das Design, welches sehr ansprechend und zeitlos ist. Fast schon könnte man die Trenntoiletten als Design-Element bezeichen. </p>
+              <p class="lead">Nicht vernachlässigt hat TROBOLO das Design, welches sehr ansprechend und zeitlos ist.
+                Fast schon könnte man die Trenntoiletten als Design-Element bezeichen. </p>
 
-            <p class="lead">Wir fühlen uns zur Nutzung einer TROBOLO Trenntoilette eingeladen und sagen "Bravo TROBOLO!".</p>
+              <p class="lead">Wir fühlen uns zur Nutzung einer TROBOLO Trenntoilette eingeladen und sagen "Bravo
+                TROBOLO!".</p>
 
               <h3 class="mt-5 mb-4">Test Kriterien</h3>
               <div class="divider my-4"></div>
@@ -173,12 +187,8 @@
 
           <div class="col-lg-4">
             <div clss="sidebar-contatct-info mt-4">
-              <img
-                src="/images/hersteller/trobolo.webp"
-                style="width: 50%"
-                alt="Kildwick"
-                class="img-fluid mx-auto d-block"
-              />
+              <img src="/images/hersteller/trobolo.webp" style="width: 50%" alt="TROBOLO"
+                class="img-fluid mx-auto d-block" />
             </div>
             <div class="sidebar-widget schedule-widget mt-5">
               <h5 class="mb-4">TROBOLO</h5>
@@ -208,14 +218,8 @@
               </div> -->
             </div>
             <div class="text-center">
-              <a
-                href="https://trobolo.com/de/"
-                target="_blank"
-                rel="nofollow noopener noreferrer"
-                title="Kildwick"
-                class="btn btn-main-2 btn-round-full"
-                >zu TROBOLO ></a
-              >
+              <a href="https://trobolo.com/de/" target="_blank" rel="nofollow noopener noreferrer" title="Kildwick"
+                class="btn btn-main-2 btn-round-full">zu TROBOLO ></a>
             </div>
           </div>
         </div>
