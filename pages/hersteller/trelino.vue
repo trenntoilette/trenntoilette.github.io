4c1bc2d0afdb8d7cefc737e8aafc8a39e9fd5e0d
trenntoilette <trenntoilette@protonmail.com>
2022-11-04 13:06:07 +0100

    deploying

---
+++ pages/hersteller/trelino.vue
@@ -0,0 +1,193 @@
+<template>
+  <div>
+    <section class="banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-md-12 col-xl-7">
+            <div class="block">
+              <div class="divider mb-3"></div>
+              <span class="text-uppercase lead letter-spacing text-light">Campingtoiletten von</span>
+              <h1 class="mb-3 mt-3 text-light">TRELINO</h1>
+
+              <p class="lead mb-4 pr-5 text-light">
+                Trelino ist ein Unternehmen mit Firmensitz und Produktionsstätte in Deutschland, welches kompakte
+                Trenntoiletten für den mobilen Einsatz wie beim Camping herstellt
+              </p>
+              <div class="btn-container">
+                <a href="#article" class="btn btn-main-2 btn-icon btn-round-full">Mehr über Trelino erfahren ></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section department-single" id="article">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="department-content mt-5">
+              <h2 class="text-md">
+                Kompakte Trenntoiletten für den mobilen Einsatz
+              </h2>
+              <div class="divider my-4"></div>
+              <p class="lead">
+                Trelino ist eine Marke der Firma Camper Active GmbH mit Sitz in Düsseldorf. Das Unternehmen hat sich auf
+                die Entwicklung von mobilen Trenntoiletten spezialisiert. So werden die Trenntoiletten hauptsächlich für
+                den Bereich Camping, Outdoor-Sport, sowie unterwegs im Van oder Wohnmobil hergestellt.
+              </p>
+              <div class="text-md h3 pt-5">
+                Produkte von Trelino
+              </div>
+              <p>Trelino baut mobile Trenntoiletten, welche man definitiv auch rein als <nuxt-link
+                  to="/campingtoilette/" title="Campingtoiletten">Campingtoiletten</nuxt-link> bezeichnen
+                kann. Jedoch stellt Trelino auch Produkte wie die Trelino Origin L her, welche für den Dauereinsatz an
+                einem Ort gedacht sind, wie z. B. im Badezimme</p>
+              <p>Das Design der Trelino ist minimalistisch und zeitlos und zieht sich über die komplette Produktpalette.
+                Somit haben die Trenntoiletten von Trelino einen Wiedererkennungswert und unterscheiden sich eigentlich
+                nur in der Größe und in der Farbe.</p>
+              <p>
+                Die Campingtoiletten von Trelino haben in unseren Tests gut abgeschnitten, lediglich der Preis ist etwas
+                höher angesetzt als wie bei anderen Herstellern.
+              </p>
+              <p>
+                Ebenfalls finden sich Selbstbausets in der Produktpalette von Trelino, Zubehör, sowie Trenneinsätze,
+                welche sich für all die Personen eignen, die eine Trenntoilette selber bauen möchten.
+              </p>
+
+              <div class="text-md h3 pt-5">
+                Zubehör von Trelino
+              </div>
+
+              <p>
+                Als Zubehör führt Trelino unter anderem Produkte wie:
+              </p>
+
+              <p>
+              <ul>
+                <li>Duftblocker</li>
+                <li>nachhaltiges Toilettenpapier</li>
+                <li>kompostierbare Beutel</li>
+                <li>
+                  <nuxt-link to="/einstreu/" title="Einstreu für Trenntoiletten">Einstreu für Trenntoiletten</nuxt-link>
+                </li>
+                <li>Feuchttuchschaum</li>
+                <li>Seifenpulver</li>
+                <li>Urinkanister</li>
+                <li>Ersatzteile für die Trelino Campingtoiletten</li>
+              </ul>
+              </p>
+
+              <p>
+                Fans von Trelino können sogar Socken mit der Aufschrift "SHIT MATTERS" erwerben, ein nettes Gimmick.
+              </p>
+
+              <p>
+                Trelino selbst schreibt auf seiner Webseite, dass Nachhaltigkeit einen sehr hohen Stellenwert im
+                Unternehmen hat. Die Produkte sind aus wiederverwendbarem Kunststoff. Lieferwege sind kurz gehalten, was
+                zu geringeren Emissionen führt.
+              </p>
+
+              <p>Der Herstellungsprozess der Trelino Trenntoiletten findet ausschließlich in Deutschland statt.</p>
+
+              <div class="text-md h3 pt-5">Fazit</div>
+
+              <p>
+                Trelino baut formschöne, mobile Trenntoiletten, welche primär für den Campingbereich entwickelt werden
+                bzw. dem mobilen Einsatz. Das Design einer Trelino zieht sich über die komplette Produktpalette und ist
+                minimalistisch und zeitlos. Die kompakten Trenntoiletten werden ausschließlich in Deutschland
+                hergestellt.
+              </p>
+              <p> Das Herzstück der Trelino Campingtoilette ist der Trenneinsatz und dieser hat in unseren Tests nie
+                enttäuscht.
+              </p>
+
+              <p>
+                Ingesamt haben die Produkte von Trelino steht's gut abgeschnitten. Die einzigen Minuspunkte vergeben wir
+                für die Wahl von Kunststoff statt Holz, sowie dem verhältnismäßig hohem Preis.
+              </p>
+
+              <h3 class="mt-5 mb-4">Test Kriterien</h3>
+              <div class="divider my-4"></div>
+              <ul class="list-unstyled department-service lead">
+                <li>
+                  <img src="/images/icons/check.webp" /> Qualität des Materials
+                </li>
+                <li><img src="/images/icons/check.webp" /> Mobilität</li>
+                <li><img src="/images/icons/check.webp" /> Sitzkomfort</li>
+                <li><img src="/images/icons/check.webp" /> Gewicht</li>
+                <li>
+                  <img src="/images/icons/check.webp" /> Geruchsentwicklung
+                </li>
+                <li><img src="/images/icons/check.webp" /> Entleerung</li>
+                <li><img src="/images/icons/check.webp" /> Hygiene</li>
+                <li><img src="/images/icons/check.webp" /> Preis</li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-lg-4">
+            <div clss="sidebar-contatct-info mt-4">
+              <img format="webp" src="/images/hersteller/trelino.png" style="width: 50%" alt="Trelino"
+                class="img-fluid mx-auto d-block" />
+            </div>
+            <div class="sidebar-widget schedule-widget mt-5">
+              <h5 class="mb-4">Trelino</h5>
+
+              <ul class="list-unstyled">
+                <li class="d-flex justify-content-between align-items-center">
+                  <a href="#">Standort</a>
+                  <span>Düsseldorf</span>
+                </li>
+                <li class="d-flex justify-content-between align-items-center">
+                  <a href="#">Website</a>
+                  <span>www.trelino.com/de/</span>
+                </li>
+                <li class="d-flex justify-content-between align-items-center">
+                  <a href="#">Telefon</a>
+                  <span>+49 211 94 25 60 59</span>
+                </li>
+                <li class="d-flex justify-content-between align-items-center">
+                  <a href="#">Email</a>
+                  <span>hello@trelino.com</span>
+                </li>
+              </ul>
+
+              <!-- <div class="sidebar-contatct-info mt-4">
+                <p class="mb-0">Preis</p>
+                <h3>579,90 EUR</h3>
+              </div> -->
+            </div>
+            <div class="text-center">
+              <a href="https://www.trelino.com/de/" target="_blank" rel="nofollow noopener noreferrer" title="Kildwick"
+                class="btn btn-main-2 btn-round-full">zu Trelino ></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Trelion",
+  head: {
+    title: "Trelino - Kompakte Trenntoiletten für den Campingbereich",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Trelino ist eine Marke der Firma Camper Active GmbH mit Sitz in Düsseldorf. Das Unternehmen hat sich auf die Entwicklung von mobilen Trenntoiletten spezialisiert.",
+      },
+      {
+        hid: "author",
+        name: "author",
+        content: "trenntoilette.net",
+      },
+    ],
+    script: [{ src: "js/main.js", type: "text/javascript", body: true }],
+  },
+};
+</script>
