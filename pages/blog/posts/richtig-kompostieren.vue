18d92a60cfb5333bbc204de895f076749ad34fce
kempo <kemal.aslan@hackuva.de>
2023-03-16 00:44:09 +0100

    deploying

--- pages/blog/posts/richtig-kompostieren.vue
+++ pages/blog/posts/richtig-kompostieren.vue
@@ -35,7 +35,7 @@
               <h2>Wie aus Kompost Humus wird</h2>
               <div class="divider mx-auto my-4"></div>
             </div>
-            <p class="lead">
+            <p class="text-justify lead">
               Bei einer Trenntoilette können die Abfälle mühelos im heimischen
               Garten kompostiert werden. Dieser Beitrag zeigt, was es bei der
               Kompostierung zu beachten gibt.
@@ -44,7 +44,7 @@
             <h2>Welche Vorteile hat eine Trenntoilette?</h2>
             <div class="divider mx-auto my-4"></div>
 
-            <p class="lead">
+            <p class="text-justify lead">
               Trenntoiletten bieten viele Vorteile – vor allem, weil sie sich
               vollkommen unabhängig verwenden lassen. Sie werden nicht nur für
               Camper genutzt, sondern finden auch in Gartenhäusern Verwendung.
@@ -59,7 +59,7 @@
             <h2>Wo entsorge ich die Trenntoilette?</h2>
             <div class="divider mx-auto my-4"></div>
 
-            <p class="lead">
+            <p class="text-justify lead">
               Trenntoiletten können ganz einfach auf dem heimischen Kompost
               entsorgt werden. Um den Zersetzungsprozess durch Mikroorganismen
               zu unterstützen, sollte beim Kompostieren auf ein optimales
@@ -67,7 +67,7 @@
               Ausscheidungen allein reichen für eine erfolgreiche Kompostierung
               nicht aus.
             </p>
-            <p class="lead">
+            <p class="text-justify lead">
               Vielmehr sollten sie mit anderen organischen Materialien gemischt
               werden. Hierzu gehören beispielsweise
             </p>
@@ -80,7 +80,7 @@
               <li>Kaffeesatz und Teebeutel,</li>
               <li>Obst und Gemüse (aber keine gekochten Küchenabfälle).</li>
             </ul>
-            <p class="lead">
+            <p class="text-justify lead">
               Das optimale Mischverhältnis von festen Ausscheidungen und
               organischen Materialien sollte im Idealfall 1:20 oder 1:25
               betragen. Durch das Mischen verschiedener Anteile wird beim
@@ -91,18 +91,21 @@
             <h2>Was darf in einer Trenntoilette entsorgt werden?</h2>
             <div class="divider mx-auto my-4"></div>
 
-            <p class="lead">
+            <p class="text-justify lead">
               Um den Zersetzungsprozess zu beschleunigen und überschüssige
               Feuchtigkeit zu binden, gibt man den festen Ausscheidungen bei
-              jedem Toilettengang eine Handvoll Einstreu hinzu. Hierbei handelt
-              es sich beispielsweise um
+              jedem Toilettengang eine Handvoll
+              <nuxt-link to="/einstreu/" title="Einstreu für Trenntoiletten"
+                >Einstreu</nuxt-link
+              >
+              hinzu. Hierbei handelt es sich beispielsweise um
             </p>
             <ul>
               <li>Kokosfasern,</li>
               <li>Sägespäne oder</li>
               <li>Rindenmulch.</li>
             </ul>
-            <p class="lead">
+            <p class="text-justify lead">
               Selbst Toilettenpapier kann problemlos in einer Trenntoilette
               entsorgt werden. Um die Handhabung und Entleerung der
               Trenntoilette zu erleichtern, wird der Feststoffbehälter mit einer
@@ -110,7 +113,7 @@
               abbaubar sein, um sie zusammen mit den Feststoffen auf den Kompost
               geben zu können.
             </p>
-            <p class="lead">
+            <p class="text-justify lead">
               Je nach Zusammensetzung der Abfälle kann die Kompostierung mehrere
               Monate in Anspruch nehmen, wobei der Zersetzungsprozess in der
               warmen Jahreszeit durch eine rasche Vermehrung der Mikroorganismen
@@ -119,7 +122,7 @@
             <h2>Kann Urin kompostiert werden?</h2>
             <div class="divider mx-auto my-4"></div>
 
-            <p class="lead">
+            <p class="text-justify lead">
               In einer Trenntoilette werden sowohl Feststoffe als auch Urin in
               einem separaten Behälter aufgefangen. Allerdings sollte der Urin
               niemals unverdünnt auf den Kompost gegeben, sondern mit Wasser
@@ -131,7 +134,7 @@
             <h2>Fazit: Trenntoiletten sind eine nachhaltige Lösung</h2>
             <div class="divider mx-auto my-4"></div>
 
-            <p class="lead">
+            <p class="text-justify lead">
               Trenntoiletten werden aufgrund ihrer einfachen Handhabung immer
               beliebter und leisten überdies einen wertvollen Beitrag in puncto
               Nachhaltigkeit. Sie kommen ohne Chemie aus und benötigen überdies
@@ -170,12 +173,61 @@ export default {
       },
     ],
   },
+  jsonld() {
+    return {
+      "@context": "https://schema.org",
+      "@type": "FAQPage",
+      mainEntity: [
+        {
+          "@type": "Question",
+          name: "Was ist eine Komposttoilette?",
+          acceptedAnswer: {
+            "@type": "Answer",
+            text: "Eine Komposttoilette ist eine Art Toilette, die menschliche Abfälle durch biologische Prozesse zersetzt, um sie in nährstoffreichen Humus zu verwandeln. Diese Toiletten benötigen keine Spülung und verhindern den Wasserverbrauch, indem sie Feststoffe und Flüssigkeiten trennen, um den Kompostierungsprozess zu beschleunigen.",
+          },
+        },
+        {
+          "@type": "Question",
+          name: "Wie funktioniert Kompostierung?",
+          acceptedAnswer: {
+            "@type": "Answer",
+            text: "Kompostierung ist der natürliche Prozess, bei dem organische Abfälle wie Lebensmittelreste, Gartenabfälle und menschliche Abfälle von Mikroorganismen zersetzt werden. Dabei entsteht ein nährstoffreiches Endprodukt namens Humus. Während des Kompostierungsprozesses wird Sauerstoff benötigt, um die aeroben Mikroorganismen zu unterstützen, die für die Zersetzung verantwortlich sind.",
+          },
+        },
+        {
+          "@type": "Question",
+          name: "Wie kann der Humus aus einer Komposttoilette verwendet werden?",
+          acceptedAnswer: {
+            "@type": "Answer",
+            text: "Der aus einer Komposttoilette gewonnene Humus kann als nährstoffreicher Dünger in Gärten und auf landwirtschaftlichen Flächen verwendet werden. Es ist jedoch wichtig, sicherzustellen, dass der Kompost vollständig zersetzt und hygienisiert wurde, um mögliche Krankheitserreger abzutöten. Es wird empfohlen, den Humus aus Komposttoiletten nicht für den Anbau von Gemüse, das roh gegessen wird, zu verwenden, um das Risiko von Krankheiten zu minimieren.",
+          },
+        },
+        {
+          "@type": "Question",
+          name: "Welche Vorteile bieten Komposttoiletten?",
+          acceptedAnswer: {
+            "@type": "Answer",
+            text: "Komposttoiletten bieten verschiedene Vorteile, wie z.B. die Verringerung des Wasserverbrauchs, da sie keine Spülung benötigen. Sie produzieren nährstoffreichen Humus, der als Dünger verwendet werden kann, und tragen zur Reduzierung von Abwasser und Umweltverschmutzung bei. Darüber hinaus können sie in Gebieten eingesetzt werden, in denen herkömmliche Abwassersysteme nicht verfügbar oder schwierig zu installieren sind.",
+          },
+        },
+        {
+          "@type": "Question",
+          name: "Wie lange dauert der Kompostierungsprozess in einer Komposttoilette?",
+          acceptedAnswer: {
+            "@type": "Answer",
+            text: "Die Dauer des Kompostierungsprozesses in einer Komposttoilette kann je nach Faktoren wie Temperatur, Feuchtigkeit, Belüftung und dem verwendeten System variieren. In der Regel dauert es mehrere Monate bis zu einem Jahr, bis der menschliche Abfall vollständig in Humus umgewandelt ist. Es ist wichtig, den Kompost regelmäßig zu überprüfen und sicherzustellen, dass er gut belüftet, feucht und hygienisiert ist, bevor er verwendet wird.",
+          },
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style scoped>
 ul {
-  text-align: center;
   list-style-position: inside;
+  /* remove dots */
+  list-style-type: none;
 }
 </style>
